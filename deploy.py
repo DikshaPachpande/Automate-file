f3e28c72491477d8b9046bcd723a1647d65ca37b
DikshaPachpande <dikshapachpande@gmail.com>
2026-04-20 10:48:00 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,26 @@
-deploy.py:
 
 import boto3
+import os
 
-s3 = boto3.client('s3')
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
+)
 
-s3.upload_file('index.html', 'automated-file-upload-on-s3', 'index.html')
+bucket_name = os.getenv("automated-file-upload-on-s3")
+
+file_name = "index.html"
+
+try:
+    s3.upload_file(
+        file_name,
+        bucket_name,
+        file_name,
+        ExtraArgs={'ContentType': 'text/html'}
+    )
+    print("✅ File uploaded successfully")
+
+except Exception as e:
+    print("❌ Upload failed:", e)
